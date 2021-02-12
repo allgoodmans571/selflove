9294666934380fe6d96ef31a3f1c777f974537ee
Alex <allgoodmans571@gmail.com>
2021-02-12 10:05:42 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { useState, useContext } from "react";
+import Loader from "react-loader-spinner";
 import Context from "./context";
 import SecondPage from "./component/SecondPage";
 import StartPage from "./component/StartPage";
-import ThirdPage from './component/ThirdPage'
+import ThirdPage from "./component/ThirdPage";
 function App() {
   const [screen, setScreen] = useState(0);
   const screenArr = [<StartPage />, <SecondPage />, <ThirdPage />];
